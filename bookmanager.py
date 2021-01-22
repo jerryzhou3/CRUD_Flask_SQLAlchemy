d97836dd7aa3698980150a91f4651f5782c5c086
Yeju Zhou <jerryzhou3@gmail.com>
2021-01-22 21:58:43 +0800

    update and delete function

--- bookmanager.py
+++ bookmanager.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,7 +14,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = db_file
 db = SQLAlchemy(app)
 
 class Book(db.Model):
-	title = db.Column(db.String(), unique = True, nullable = False, primary_key = True)
+	title = db.Column(db.String(80), unique = True, nullable = False, primary_key = True)
 	price = db.Column(db.Integer(), unique = False, nullable = False, primary_key = False)
 
 	def __repr__(self):
@@ -30,6 +31,28 @@ def home():
 	books = Book.query.all()
 	return render_template('home.html', books = books)
 
+@app.route("/update", methods = ["GET", "POST"])
+
+def update():
+	oldtitle = request.form.get("oldtitle")
+	oldprice = request.form.get("oldprice")
+	newtitle = request.form.get("newtitle")
+	newprice = request.form.get("newprice")
+	book = Book.query.filter_by(title = oldtitle).first()
+	book.title = newtitle
+	book.price = newprice
+	db.session.commit()
+	return redirect("/")
+
+
+@app.route("/delete", methods = ["GET", "POST"])
+
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title = title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
 	app.run(host = '0.0.0.0', debug = True)
